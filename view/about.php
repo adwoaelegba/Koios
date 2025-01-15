2d798d75b8ce4048cb92def3bbe90dbd1e24be7c
Nana Adwoa <naelegba@gmail.com>
2025-01-15 10:28:47 +0000

    about first section 1

---
+++ view/about.php
@@ -0,0 +1,31 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Koios-About</title>
+    <!-- Font Awesome CDN link  -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.2/css/all.min.css">
+    <link rel="stylesheet" href="../css/aboutCSS.css">
+
+    <!--JQuery libraries -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+</head>
+<body>
+  
+    <header>
+    <img src="../images/koios.png" alt="Logo" class="logo">
+    </header>
+
+    <!-- Introduce the tool guide -->
+  <div class="section active" id="section1">
+    <h1>Hi, I am Koios!</h1>
+    <p>
+      The titan of knowledge and your privacy agreement summary guide. I'll help you understand privacy terms effortlessly.
+    </p>
+  </div>
+
+    
+</body>
+</html>
