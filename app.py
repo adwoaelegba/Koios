77894949c10d8ded9d0c689dd22d8f3ee6e73763
Nana Adwoa <147147969+adwoaelegba@users.noreply.github.com>
2025-03-29 14:26:53 +0000

    Update app.py

--- app.py
+++ app.py
@@ -150,5 +150,5 @@ def koios_summarize():
     summary = process_summary(url)
     return jsonify(summary)
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000,debug=True)
